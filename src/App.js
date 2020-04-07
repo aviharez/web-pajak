9566d108b22ddd2ca29c2014268a7a3884b2306e
Bee Technology <beet@gobib.site>
2020-04-07 13:45:26 +0700

    server config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, HashRouter } from 'react-router-dom'
 
 import Form from './pages/Form'
 import Preview from './pages/Preview'
 
 export default function App() {
   return (
+   <HashRouter>
     <Switch>
-      <Route exact path="/" component={Form} />
+      <Route exact={true} path="/" component={Form} />
       <Route path="/preview" component={Preview} />
     </Switch>
+   </HashRouter>
   )
 }
