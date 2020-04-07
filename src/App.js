91ab07c12e38c69d6324d56083c498cf862c8862
Syifa Nurzain <elzainsyifa@gmail.com>
2020-04-07 13:47:15 +0700

    aborted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 
 import Form from './pages/Form'
 import Preview from './pages/Preview'
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Form} />
-      <Route path="/preview" component={Preview} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Form} />
+        <Route path="/preview" component={Preview} />
+      </Switch>
+    </BrowserRouter>
   )
 }
